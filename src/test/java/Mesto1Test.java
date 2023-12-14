88f2675adbf8949103c1f4ce320688efa6463ce5
Olga Teshchina <O.T89204217366@yandex.ru>
2023-12-14 22:24:27 +0300

    Initial commit

---
+++ src/test/java/Mesto1Test.java
@@ -0,0 +1,73 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.RestAssured;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.*;
+
+import static io.restassured.RestAssured.given;
+import io.*;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class Mesto1Test {
+
+    String bearerToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTJlZjFmMTk0MzA0YzAwM2Q5MjliZGUiLCJpYXQiOjE3MDI1ODE2NTIsImV4cCI6MTcwMzE4NjQ1Mn0.lx8IUPWVVMEu7S5dGLb5Pde1LeOto-eqALijKOro3tM";
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "https://qa-mesto.praktikum-services.ru";
+    }
+
+    @Test
+    @DisplayName("Add a new photo")
+    @Description("This test is for adding a new photo to Mesto.")
+    public void addNewPhoto() {
+        given()
+                .header("Content-type", "application/json") // Передаём Content-type в заголовке для указания типа файла
+                .auth().oauth2(bearerToken) // Передаём токен для аутентификации
+                .body("{\"name\":\"Москва\",\"link\":\"https://code.s3.yandex.net/qa-automation-engineer/java/files/paid-track/sprint1/photoSelenium.jpg\"}") // Формируем тело запроса
+                .post("/api/cards") // Делаем POST-запрос
+                .then().statusCode(201); // Проверяем код ответа
+    }
+
+    @Test
+    @DisplayName("Like the first photo")
+    @Description("This test is for liking the first photo on Mesto.")
+    public void likeTheFirstPhoto() {
+        String photoId = getTheFirstPhotoId();
+
+        likePhotoById(photoId);
+        deleteLikePhotoById(photoId);
+    }
+
+    @Step("Take the first photo from the list")
+    private String getTheFirstPhotoId() {
+        // Получение списка фотографий и выбор первой из него
+        return given()
+                .auth().oauth2(bearerToken) // Передаём токен для аутентификации
+                .get("/api/cards") // Делаем GET-запрос
+                .then().extract().body().path("data[0]._id"); // Получаем ID фотографии из массива данных
+    }
+
+    @Step("Like a photo by id")
+    private void likePhotoById(String photoId) {
+        // Лайк фотографии по photoId
+        given()
+                .auth().oauth2(bearerToken) // Передаём токен для аутентификации
+                .put("/api/cards/{photoId}/likes", photoId) // Делаем PUT-запрос
+                .then().assertThat().statusCode(200); // Проверяем, что сервер вернул код 200
+    }
+
+    @Step("Delete like from the photo by id")
+    private void deleteLikePhotoById(String photoId) {
+        // Снять лайк с фотографии по photoId
+        given()
+                .auth().oauth2(bearerToken) // Передаём токен для аутентификации
+                .delete("/api/cards/{photoId}/likes", photoId) // Делаем DELETE-запрос
+                .then().assertThat().statusCode(200); // Проверяем, что сервер вернул код 200
+    }
+
+}
